b28047c049656f26eb6fe4e3d256312dd381b128
Edgar Torres <65181657+torres-dev@users.noreply.github.com>
2021-04-23 15:18:39 -0300

    Qual a média aritmética?

---
+++ Exercicio 2 v2.py
@@ -0,0 +1,6 @@
+nota1 = float( input( "informe a nota 1") )
+nota2 = float( input( "informe a nota 2") )
+nota3 = float( input( "informe a nota 3") )
+nota4 = float( input( "informe a nota 4") ) 
+média = (nota1+nota2+nota3+nota4) / 4
+print( "A média aritmetica é", média)
